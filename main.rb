929a7779814d53ddf801523181a38a10afdd7400
Roman Huk <romanguk@zoho.com>
2022-07-08 23:31:56 +0300

    execute script

--- main.rb
+++ main.rb
@@ -1,15 +1,28 @@
 require 'faraday'
 require 'pry'
-require 'open-uri'
+require 'pty'
+require 'expect'
 
 MODPACK_ID = 95
 
 # call get response
 response = Faraday.get('https://api.modpacks.ch/public/modpack/95')
 body = JSON.parse(response.body, symbolize_names: true)
-id = body[:versions].sort { |v| v[:updated] }.first.fetch(:id) # get katest version id
+ver = body[:versions].sort { |v| v[:updated] }.first
+id = ver.fetch(:id) # get latest version id
 
 file_name = "serverinstall_#{MODPACK_ID}_#{id}.sh"
+ver_dir = "versions/#{id}"
 
-`wget http://api.modpacks.ch/public/modpack/#{MODPACK_ID}/#{id}/server/linux -O server/#{file_name}`
+`wget http://api.modpacks.ch/public/modpack/#{MODPACK_ID}/#{id}/server/linux -O #{ver_dir}/#{file_name}`
 `sudo chmod +x server/#{file_name}`
+`mkdir -p #{ver_dir}`
+
+PTY.spawn("./#{ver_dir}/#{file_name}") do |reader, writer|
+  reader.expect(/Where would you like to install the server? [current directory]/)
+  writer.puts
+  reader.expect(/Path . already exists - still want to install? (y/n) [y]/)
+  writer.puts
+  reader.expect(/Continuing will install/)
+  writer.puts
+end

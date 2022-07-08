68bee1424ccd28e98ce366235577c13b84fc43dd
Roman Huk <romanguk@zoho.com>
2022-07-09 00:54:43 +0300

    added persister content linking

--- main.rb
+++ main.rb
@@ -16,7 +16,16 @@ ver_dir = "versions/#{ver[:name]}"
 
 `mkdir -p #{ver_dir} &&
   wget http://api.modpacks.ch/public/modpack/#{MODPACK_ID}/#{id}/server/linux -O #{ver_dir}/#{file_name} &&
-  sudo chmod +x #{ver_dir}/#{file_name}`
+  sudo chmod +x #{ver_dir}/#{file_name} &&
+  ln -s #{ENV['PWD']}/persisted/world #{ENV['PWD']}/#{ver_dir}/world &&
+  ln -s #{ENV['PWD']}/persisted/banned-ips.json #{ENV['PWD']}/#{ver_dir}/banned-ips.json &&
+  ln -s #{ENV['PWD']}/persisted/banned-players.json #{ENV['PWD']}/#{ver_dir}/banned-players.json &&
+  ln -s #{ENV['PWD']}/persisted/ops.json #{ENV['PWD']}/#{ver_dir}/ops.json &&
+  ln -s #{ENV['PWD']}/persisted/whitelist.json #{ENV['PWD']}/#{ver_dir}/whitelist.json &&
+  ln -s #{ENV['PWD']}/persisted/eula.txt #{ENV['PWD']}/#{ver_dir}/eula.txt &&
+  ln -s #{ENV['PWD']}/persisted/user_jvm_args.txt #{ENV['PWD']}/#{ver_dir}/user_jvm_args.txt &&
+  exit 0
+`
 
 PTY.spawn("cd #{ver_dir} && ./#{file_name}") do |reader, writer|
   writer.printf("\n")
@@ -26,3 +35,6 @@ PTY.spawn("cd #{ver_dir} && ./#{file_name}") do |reader, writer|
   reader.each {|line| puts line}
 rescue Errno::EIO
 end
+
+`rm -rf #{ENV['PWD']}/current &&
+  ln -s #{ENV['PWD']}/#{ver_dir} #{ENV['PWD']}/current`
